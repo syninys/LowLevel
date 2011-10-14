a3a17571f459dfdd823088a01f11dd0f20c63d27
Syn Inys <syninys@gmail.com>
2011-10-14 22:04:10 +0100

    Better infrastructure for pushing bytes through (usb)serial

--- Arduino/pump.sh
+++ Arduino/pump.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-./echoBytes $* > /dev/tty.usbmodem411
+# Push a set of raw bytes to a serial terminal
+
+TTY=/dev/tty.usbmodem411
+
+if [ ! -f ./echoBytes ] 
+    then
+    gcc echoBytes.c -o echoBytes
+fi
+
+./echoBytes $* > $TTY
+
